6762f0be3bd28d9f0fe7a70a6562b48251eb442f
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 04:47:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-    programmer_hash[1][0] = alans_new_info
+    programmer_hash[:alan_kay][:known_for] => alans_new_info
+  
     end
 
 def adding_to_dennis
