308da25cce8cb3b91f46e4f8823c8573cb264dd7
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 04:53:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+      programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     language = programmer_hash[:dennis_ritchie][:languages]
+     language[0]
 end
 
 def adding_matz
@@ -66,6 +72,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		   :yukihiro_matsumoto => {
+         :known_for => "Ruby",
+         :languages => ["LISP", "C"]
+         },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,8 +89,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
 end
 
 def changing_alan
@@ -101,9 +109,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
-end
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    return programmer_hash
+  
+    end
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash
 end
