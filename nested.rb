364b45bec53c6402fd85eaa4c15a8a56e2fd7cab
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-21 04:40:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
      
-     return programmer_hash[:alan_kay][:known_for][0]
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
